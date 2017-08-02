01b7a33a5363dcf9a19e00c420d21c530ce28d0f
ByeongJin Kim <playfulbj@gmail.com>
2017-08-02 09:11:58 +0900

    component

---
+++ js/source/components/Actions.js
@@ -0,0 +1,30 @@
+import React, { Component, PropTypes } from 'react';
+
+const Actions = props =>
+  <div className="Actions">
+    <span
+      tabIndex="0"
+      className="ActionsInfo"
+      title="More info"
+      onClick={props.onAction.bind(null, 'info')}>&#8505;</span>
+    <span
+      tabIndex="0"
+      className="ActionsEdit"
+      title="edit"
+      onClick={props.onAction.bind(null, 'edit')}>&#10000;</span>
+    <span
+      tabIndex="0"
+      className="ActionsDelete"
+      title="Delete"
+      onClick={props.onAction.bind(null, 'delete')}>x</span>
+  </div>
+
+Actions.propTypes = {
+  onAction: PropTypesfunc,
+};
+
+Actions.defaultProps = {
+  onAction: () => { },
+};
+
+export default Actions;
